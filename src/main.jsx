6b19e075934a0cb4ad01ed1b3744419ad045f58d
Cody Somerville <cody.somerville@gmail.com>
2025-05-12 23:18:16 -0500

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import TankBlitzGame from './TankBlitzGame';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TankBlitzGame />
+  </React.StrictMode>
+);
